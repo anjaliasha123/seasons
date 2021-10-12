78cc9b0c05948d3d7bd2d1e6445198b8803fd165
Anjali Asha Jacob <66244077+anjaliasha123@users.noreply.github.com>
2021-10-12 22:58:33 +0530

    state changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
+//import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  return (
-    <div>
-      Hi Tehere
-      <SeasonDisplay />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lattitude: null, errorMessage: "" };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lattitude: position.coords.latitude });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
+    );
+  }
+  render() {
+    if (this.state.errorMessage && !this.state.lattitude) {
+      return <div>Error : {this.state.errorMessage}</div>;
+    }
+    if (this.state.lattitude && !this.state.errorMessage) {
+      return <div>Latitutde : {this.state.lattitude}</div>;
+    }
+
+    return <div>Loading Content....</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
